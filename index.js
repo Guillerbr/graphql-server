810eb7a2c620b3ead8d7470db1f5616a50ee1390
Guillerbr <ghminvest@gmail.com>
2019-07-09 03:49:29 -0300

    add router,build and express

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const expressGraphQL = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 
+const schema = buildSchema('
+  type Query {
+        user: String
+    } 
+');
 
+cosnt name = "Caio";
+
+const resolvers = {
+    user: () {
+        return name;
+    }
+};
 
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+
+}));
 app.listen(3000);
