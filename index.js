58013f125625f9c23b24c7abd6edd407bc758e8e
Guillerbr <ghminvest@gmail.com>
2019-07-09 03:42:29 -0300

    add schema graphql

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+
+
+
+const app = express();
+
+app.listen(3000);
